d535fb7becd8ac797abca68e69b9f0d61ccbaf10
Hannah Kim <devhkim13@gmail.com>
2023-10-16 17:08:17 -0700

    Refactor code to set up new approach
    This commit refactors the modules to follow a new approach using nodes (i.e., Knight factories). Creating links between each possible move would help determine the knight's shortest path in future implementations

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
 import knightMoves from './scripts/knightMoves.js';
 
-knightMoves([0, 0], [3, 3]);
+knightMoves([3, 3], [4, 3]);
---
+++ scripts/knight.js
@@ -0,0 +1,25 @@
+const Knight = () => {
+  const position = null;
+  const move1 = null;
+  const move2 = null;
+  const move3 = null;
+  const move4 = null;
+  const move5 = null;
+  const move6 = null;
+  const move7 = null;
+  const move8 = null;
+
+  return {
+    position,
+    move1,
+    move2,
+    move3,
+    move4,
+    move5,
+    move6,
+    move7,
+    move8,
+  };
+};
+
+export default Knight;
